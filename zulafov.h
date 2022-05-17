e86fa509b29fcf1708d223ab4419e6bc4d2d86a6
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2022-05-17 21:12:14 +0300

    zula
    zula

---
+++ zulafov.h
@@ -0,0 +1,5 @@
+ImDrawList* ZulaDraw = ImGui::GetBackgroundDrawList();
+ZulaDraw->AddCircle(ImVec2(1280 / 2, 800 / 2) // oyununuzun pencere boyutu kadar olması gerekmekte
+, AımFov, ImColor(2.0f, 0.0f, 0.0f, 8.0f), 12, 1.0f);
+
+int AımFOV = 90;
